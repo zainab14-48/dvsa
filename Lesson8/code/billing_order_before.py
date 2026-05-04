16023b86cfc69f91a68f31b07158c67745d09e81
Zahraaalmadeh <s202254580@kfupm.edu.sa>
2026-05-04 19:28:58 +0300

    zahraa

---
+++ Lesson8/code/billing_order_before.py
@@ -0,0 +1,164 @@
+import json
+import urllib3
+import boto3
+import os
+import time
+import decimal
+from decimal import Decimal
+
+
+# status list
+# -----------
+# 100: open
+# 110: payment-failed
+# 120: paid
+# 200: processing
+# 210: shipped
+# 300: delivered
+# 500: cancelled
+# 600: rejected
+
+def lambda_handler(event, context):
+    print(json.dumps(event))
+
+    class DecimalEncoder(json.JSONEncoder):
+        def default(self, o):
+            if isinstance(o, decimal.Decimal):
+                if o % 1 > 0:
+                    return float(o)
+                else:
+                    return int(o)
+            return super(DecimalEncoder, self).default(o)
+
+    orderId = event["orderId"]
+    userId = event["user"]
+    http = urllib3.PoolManager()
+
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(os.environ["ORDERS_TABLE"])
+
+    response = table.get_item(
+        Key={
+            "orderId": orderId,
+            "userId": userId
+        },
+        AttributesToGet=["orderId", "orderStatus", "itemList"]
+    )
+
+    if "Item" not in response:
+        return {"status": "err", "msg": "could not find order"}
+
+    order = response["Item"]
+
+    data_dict = []
+    for key, value in order.get("itemList", {}).items():
+        data_dict.append({
+            "itemId": key,
+            "quantity": int(value)
+        })
+
+    data = json.dumps(data_dict, cls=DecimalEncoder)
+
+    # GET TOTAL FOR BILLING
+    url = os.environ["GET_CART_TOTAL"]
+    clen = len(data)
+
+    req = http.request(
+        "POST",
+        url,
+        body=data,
+        headers={
+            "Content-Type": "application/json",
+            "Content-Length": str(clen)
+        }
+    )
+
+    res = json.loads(req.data.decode("utf-8"))
+
+    cartTotal = float(res["total"])
+    missings = res["missing"] if "missing" in res else {}
+
+    # SEND BILLING DATA TO PAYMENT
+    url = os.environ["PAYMENT_PROCESS_URL"]
+    data = json.dumps(event["billing"])
+    clen = len(data)
+
+    req = http.request(
+        "POST",
+        url,
+        body=data,
+        headers={
+            "Content-Type": "application/json",
+            "Content-Length": str(clen)
+        }
+    )
+
+    res = json.loads(req.data.decode("utf-8"))
+    ts = int(time.time())
+
+    if res["status"] == 110:
+        return {"status": "err", "msg": "invalid payment details"}
+
+    elif res["status"] == 120:
+        key = {
+            "orderId": orderId,
+            "userId": userId
+        }
+
+        update_expression = (
+            "SET orderStatus = :orderstatus, "
+            "paymentTS = :paymentTS, "
+            "totalAmount = :total, "
+            "confirmationToken = :token"
+        )
+
+        TWOPLACES = Decimal("0.01")
+
+        expression_attributes = {
+            ":orderstatus": res["status"],
+            ":paymentTS": ts,
+            ":total": Decimal(str(cartTotal)).quantize(TWOPLACES),
+            ":token": res["confirmation_token"]
+        }
+
+        if missings:
+            new_item_list = {}
+            response = table.get_item(Key=key)
+            items = response.get("Item", {}).get("itemList", {})
+
+            for item in items:
+                new_item_list[item] = items[item] - missings[item] if missings.get(item) else items[item]
+
+            expression_attributes[":il"] = new_item_list
+            update_expression += ", itemList = :il"
+
+        try:
+            table.update_item(
+                Key=key,
+                UpdateExpression=update_expression,
+                ExpressionAttributeValues=expression_attributes
+            )
+
+            sqs = boto3.client("sqs")
+            sqs.send_message(
+                QueueUrl=os.environ["SQS_URL"],
+                MessageBody=json.dumps({
+                    "orderId": orderId,
+                    "userId": userId
+                }),
+                DelaySeconds=10
+            )
+
+            return {
+                "status": "ok",
+                "amount": float(cartTotal),
+                "token": res["confirmation_token"],
+                "missing": missings
+            }
+
+        except Exception as e:
+            print("Update order error:", str(e))
+            return {"status": "err", "msg": "unknown error"}
+
+    else:
+        return {"status": "err", "msg": "could not process payment"}
